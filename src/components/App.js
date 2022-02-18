3fe0f8d0ae9f3e76d6ad1f9e5c240d6c3828c0ae
Reid Sheppard <reidsheppard@ReidMBP.lan>
2022-02-18 11:16:41 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import Web3 from 'web3';
+import NavBar from './NavBar'
+import './App.css';
+
+
+class App extends Component {
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({account:accounts[0]})
+    const ethBalance = web3.eth.getBalance(this.state.account);
+    this.setState({ethBalance:ethBalance})
+    console.log(this.state.ethBalance)
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  constructor(props){
+    super(props);
+    this.state = {
+      account:'',
+      ethBalance:'0'
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar account={this.state.account}></NavBar>
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto">
+                <h1>Hello World</h1>
+                <h1>joe</h1>
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
